2a9bf0e951b37a11c53f8616953c8e3069917427
Timothy Geary <tdgeary@gmail.com>
2017-10-02 17:54:28 -0400

    Adds an ArrayList of loans and getters and setters for it

--- src/main/java/com/timothygeary/Account.java
+++ src/main/java/com/timothygeary/Account.java
@@ -1,6 +1,30 @@
 package com.timothygeary;
 
+import java.util.ArrayList;
+
 /** Keeps track of all loans and repeatable transactions */
 public class Account{
+    private ArrayList<Loan> loans;
 
+    public Account(){
+        loans = new ArrayList<>();
+    }
+
+    public ArrayList<Loan> getLoans(){ return loans; }
+
+    public void addLoan(Loan newLoan){ loans.add(newLoan); }
+
+    public void addLoans(ArrayList<Loan> newLoans){
+        for(Loan newLoan : newLoans){
+            addLoan( newLoan );
+        }
+    }
+
+    public void removeLoan(String name){
+        for(int i=0; i < loans.size(); i++){
+            if(loans.get(i).getName().equals( name )) loans.remove(i);
+        }
+    }
+
+    public void clearLoans(){ loans.clear(); }
 }
