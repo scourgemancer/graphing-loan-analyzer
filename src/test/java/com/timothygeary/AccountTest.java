9f223e9b3a910639327c529e70257a3fcc93ab82
Timothy Geary <tdgeary@gmail.com>
2017-10-03 08:52:12 -0400

    Populates the Account testing class

--- src/test/java/com/timothygeary/AccountTest.java
+++ src/test/java/com/timothygeary/AccountTest.java
@@ -1,4 +1,63 @@
 package com.timothygeary;
 
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+
+import static org.testng.Assert.assertEquals;
+
 public class AccountTest{
+    @Test
+    public void testGetLoans(){
+        Account test = new Account();
+        ArrayList<Loan> empty =  new ArrayList<>();
+        assertEquals(test.getLoans(), empty);
+    }
+
+    @Test(dependsOnMethods = "testGetLoans")
+    public void testAddLoan(){
+        Account test = new Account();
+        Loan testLoan = new Loan("Test1");
+        test.addLoan( testLoan );
+        ArrayList<Loan> loanList = new ArrayList<>();
+        loanList.add( testLoan );
+        assertEquals(test.getLoans(), loanList);
+    }
+
+    @Test(dependsOnMethods = { "testAddLoan", "testGetLoans" } )
+    public void testAddLoans(){
+        Account test = new Account();
+        ArrayList<Loan> loans = new ArrayList<>();
+        loans.add(new Loan("Loan1"));
+        loans.add(new Loan("Loan2"));
+        loans.add(new Loan("Loan3"));
+        test.addLoans( loans );
+        assertEquals(test.getLoans(), loans);
+    }
+
+    @Test(dependsOnMethods = { "testAddLoans", "testGetLoans" } )
+    public void testRemoveLoan(){
+        Account test = new Account();
+        ArrayList<Loan> loans = new ArrayList<>();
+        loans.add(new Loan("Loan1"));
+        loans.add(new Loan("Loan2"));
+        loans.add(new Loan("Loan3"));
+        test.addLoans( loans );
+        test.removeLoan("Loan2");
+        loans.remove(1);
+        assertEquals(test.getLoans(), loans);
+    }
+
+    @Test(dependsOnMethods = { "testAddLoans", "testGetLoans" } )
+    public void testClearLoans(){
+        Account test = new Account();
+        ArrayList<Loan> loans = new ArrayList<>();
+        loans.add(new Loan("Loan1"));
+        loans.add(new Loan("Loan2"));
+        loans.add(new Loan("Loan3"));
+        test.addLoans( loans );
+        test.clearLoans();
+        loans.clear();
+        assertEquals(test.getLoans(), loans);
+    }
 }
