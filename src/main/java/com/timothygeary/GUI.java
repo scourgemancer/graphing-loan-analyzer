150767e87ec767f9b13e5f554066d2f1b6e68e6a
Timothy Geary <tdgeary@gmail.com>
2017-10-05 00:25:40 -0400

    Opens a window for the application with a set title

--- src/main/java/com/timothygeary/GUI.java
+++ src/main/java/com/timothygeary/GUI.java
@@ -1,6 +1,21 @@
 package com.timothygeary;
 
-/** Aptly named class responsible for the Graphical User Interface */
-public class GUI{
+import javafx.application.Application;
+import javafx.scene.image.Image;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
 
+/** Aptly named class responsible for the Graphical User Interface */
+public class GUI extends Application{
+    @Override
+    public void start(Stage stage) throws Exception{
+        stage.setHeight( Screen.getPrimary().getBounds().getHeight() * 7 / 8 );
+        stage.setWidth( Screen.getPrimary().getBounds().getWidth() / 2 );
+        stage.setTitle("Graphing Loan Analyzer");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
 }
